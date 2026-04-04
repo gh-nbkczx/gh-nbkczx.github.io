24e6c83d5c58ba7a6fdd500139362b510fe0b5bc
zycdzyj <mzycdzyj@163.com>
2026-04-04 15:53:11 +0800

    First upload

---
+++ assets/js/main_2.ts
@@ -0,0 +1,6 @@
+const hamburger: HTMLElement | null = document.querySelector('#hamburgerBtn_about');
+const navMenu: HTMLElement | null = document.querySelector('#navMenu_about');
+
+hamburger?.addEventListener('click', () => {
+    navMenu?.classList.toggle('active');
+});
